9c1a4ec5df3794c0ecd7092607ebd0fa4ad2c38b
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-08 01:26:01 -0300

    feat: std::io and conditions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
+use std::io;
+
+fn convert_to_int(input: &String) -> i32 {
+    let x = input.trim().parse::<i32>().unwrap();
+
+    return x;
+}
+
 fn main() {
-    let x: u64 = 23; // not accept negative numbers
-    let y: i64 = 23; // accept negative numbers too
-    let f: f64 = 6.3;
-    let b: bool = false;
+    let mut first_number = String::new();
+    io::stdin()
+        .read_line(&mut first_number)
+        .expect("Error on read first_number");
 
-    let mut name = "Marcos";
+    let mut second_number = String::new();
+    io::stdin()
+        .read_line(&mut second_number)
+        .expect("Error on read second_number");
 
-    name = "Daniel";
-
-    print!("test {}", b);
+    if convert_to_int(&first_number) > convert_to_int(&second_number) {
+        println!(
+            "The number {} is greater than {}",
+            first_number, second_number
+        );
+    } else if convert_to_int(&first_number) == convert_to_int(&second_number) {
+        println!(
+            "The number {} is equal than {}",
+            first_number, second_number
+        );
+    } else {
+        println!("The number {} is less than {}", first_number, second_number);
+    }
 }
