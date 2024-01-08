2d821abb00761a33421d975d8cc6af2845ded7fe
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-08 01:07:59 -0300

    fix: variables comments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let x: u64 = 23; // not accept negative numbers
-    let y: i64 = 23; // not accept negative numbers
+    let y: i64 = 23; // accept negative numbers too
     let f: f64 = 6.3;
     let b: bool = false;
 
