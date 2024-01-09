3653074e282f8c043dcfea3c8c7da4cd40a6c9c8
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-09 02:56:55 -0300

    chore: sum numbers typed using while

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,26 @@
 use std::io;
 
 fn convert_to_int(input: &String) -> i32 {
-    let x = input.trim().parse::<i32>().unwrap();
+    let result = input.trim().parse::<i32>().unwrap();
 
-    return x;
+    result
 }
 
 fn main() {
-    let mut first_number = String::new();
-    io::stdin()
-        .read_line(&mut first_number)
-        .expect("Error on read first_number");
+    let mut sum: i32 = 0;
 
-    let mut second_number = String::new();
+    let mut input_value: String = String::new();
     io::stdin()
-        .read_line(&mut second_number)
-        .expect("Error on read second_number");
+        .read_line(&mut input_value)
+        .expect("Erro on read input_number");
 
-    if convert_to_int(&first_number) > convert_to_int(&second_number) {
-        println!(
-            "The number {} is greater than {}",
-            first_number, second_number
-        );
-    } else if convert_to_int(&first_number) == convert_to_int(&second_number) {
-        println!(
-            "The number {} is equal than {}",
-            first_number, second_number
-        );
-    } else {
-        println!("The number {} is less than {}", first_number, second_number);
+    let mut input_value_i32: i32 = convert_to_int(&input_value);
+
+    while input_value_i32 != 0 {
+        let r = input_value_i32 % 10;
+        sum += r;
+        input_value_i32 /= 10;
     }
+
+    println!("{}", sum);
 }
