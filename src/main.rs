afd295f9f376d4c66b633668bd167b32b9bfa84c
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-12 01:49:58 -0300

    chore: forcing types

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,17 @@ fn max(a: i32, b: i32) -> i32 {
     return b;
 }
 
+fn force_type_example(a: f32, b: i128) -> f32 {
+    let x = 10.1 * a + b as f32;
+
+    return x;
+
+    // return 10 as f32;
+}
+
 fn main() {
     println!("{}", double(2));
     println!("{}", max(1, 2));
+
+    println!("{}", force_type_example(20.0, 1))
 }
