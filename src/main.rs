f4fd84192dce9618e686e9e0392ab93ce01bfeed
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-12 01:41:47 -0300

    chore: basics functions

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,16 @@
-use std::io;
+fn double(number: i32) -> i32 {
+    number * 2
+}
 
-fn convert_to_int(input: &String) -> i32 {
-    let result = input.trim().parse::<i32>().unwrap();
+fn max(a: i32, b: i32) -> i32 {
+    if a > b {
+        return a;
+    }
 
-    result
+    return b;
 }
 
 fn main() {
-    let mut mediums: String = String::new();
-    io::stdin()
-        .read_line(&mut mediums)
-        .expect("Error on read mediums");
-
-    let mut recovery_students: i128 = 0;
-    let mut i = 0;
-
-    while convert_to_int(&mediums) > i {
-        let mut student_medium = String::new();
-        io::stdin()
-            .read_line(&mut student_medium)
-            .expect("Error on read student_medium");
-
-        i += 1;
-
-        if convert_to_int(&student_medium) >= 3 && convert_to_int(&student_medium) < 6 {
-            recovery_students += 1;
-        }
-    }
-
-    println!("Has {} recovery students", recovery_students);
+    println!("{}", double(2));
+    println!("{}", max(1, 2));
 }
