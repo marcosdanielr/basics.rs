fc23c9eeb129c840936aee364f857baf33b428ad
marcosdanielr <marcosdaniel99@protonmail.com>
2024-01-12 01:57:05 -0300

    chore: for loop

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,11 @@
-fn double(number: i32) -> i32 {
-    number * 2
-}
+fn main() {
+    let animals = vec!["bunny", "dog", "cat", "rabbit", "rat"];
 
-fn max(a: i32, b: i32) -> i32 {
-    if a > b {
-        return a;
+    for animal in animals {
+        println!("{}", animal);
     }
 
-    return b;
-}
-
-fn force_type_example(a: f32, b: i128) -> f32 {
-    let x = 10.1 * a + b as f32;
-
-    return x;
-
-    // return 10 as f32;
-}
-
-fn main() {
-    println!("{}", double(2));
-    println!("{}", max(1, 2));
-
-    println!("{}", force_type_example(20.0, 1))
+    // for i in 0..10 {
+    //     println!("{}", i);
+    // }
 }
